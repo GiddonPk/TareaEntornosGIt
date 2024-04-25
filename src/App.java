3bef8d93fe92c77289c606cfceb04682d4f930c7
GiddonPk <jhg0015@alu.medac.es>
2024-04-25 20:00:22 +0200

    feature commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, develop!");
-        System.out.println("commit 2!");
+        System.out.println("Hello, feature!");
+        
     }
 }
