afd83face69d6190558bc70e904778456d865311
GiddonPk <jhg0015@alu.medac.es>
2024-04-25 20:02:59 +0200

    comit 3 after merge

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, master!");
         System.out.println("commit 2!");
+        System.out.println("commit 3 after merge!");
         //merge master con hotfix
     }
 }
