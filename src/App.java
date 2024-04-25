d3e2798c488bc3e4412be9c92e34ba9e39a2ec82
GiddonPk <jhg0015@alu.medac.es>
2024-04-25 20:06:26 +0200

    commit 1 release

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         System.out.println("Hello release!");
+        System.out.println("commit 1!");
 
     }
 }
