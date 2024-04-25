05dcd824529305f8c10bbca1726f97ba8dc1302c
GiddonPk <jhg0015@alu.medac.es>
2024-04-25 20:05:55 +0200

    commit release

--- src/App.java
+++ src/App.java
@@ -1,11 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, develop!");
-        System.out.println("commit 2!");
-        System.out.println("commit 3!");
-        // merge con hotfix
-        System.out.println("commit 4 after merge!");
-        // merge develop con feature
+        
+        System.out.println("Hello release!");
 
     }
 }
